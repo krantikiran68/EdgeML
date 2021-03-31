ee5abf20e95849b80981f5a13c9acbbbcc1e412c
G Rahul Kranti Kiran <krantikiran.68@gmail.com>
2021-03-31 11:25:23 +0530

    Added the FastGRNNPredictor code unrolled version

--- examples/tf/FastCells/fastcell_example.py
+++ examples/tf/FastCells/fastcell_example.py
@@ -13,12 +13,47 @@ from edgeml_tf.graph.rnn import UGRNNLRCell
 from edgeml_tf.graph.rnn import GRULRCell
 from edgeml_tf.graph.rnn import LSTMLRCell
 
+from tensorflow.python.ops import math_ops
+import numpy as np
+
+class FastGRNNPredictor(tf.keras.Model):
+
+    def __init__(self, model, FC, FCbias):
+        super(FastGRNNPredictor, self).__init__(name='FastGRNNPredictor')
+        self.model = model
+
+        self.FC = FC
+        self.FCbias = FCbias
+
+    def call(self, x):
+        H = tf.zeros_initializer()(shape=[1,100], dtype=tf.float32)
+        
+        cond = lambda i, x, H: i<99
+        def while_body(i, x, H):
+            XX = x[32*i:32*(i+1), :]
+            a = math_ops.matmul( math_ops.matmul(XX, self.model.W1, transpose_a=True), self.model.W2)
+            b = math_ops.matmul( math_ops.matmul(H, self.model.U1), self.model.U2)
+            c = a + b
+            g = math_ops.sigmoid( c + self.model.bias_gate)
+            h = math_ops.tanh(c + self.model.bias_update)
+            H = (g * H) + (math_ops.sigmoid(self.model.zeta) * (1.0 - g) + math_ops.sigmoid(self.model.nu)) * h
+            return i+1, x, H
+        
+        i = 0
+        _, H = tf.while_loop(cond, while_body, loop_vars=[i, x, H])
+        score = math_ops.matmul(H, self.FC) + self.FCbias
+
+        return tf.math.argmax(score)
+            
+        
+
+
 
 def main():
     # Fixing seeds for reproducibility
     tf.random.set_seed(42)
     np.random.seed(42)
-    tf.compat.v1.disable_eager_execution()
+    # tf.compat.v1.disable_eager_execution()
 
 
     # Hyper Param pre-processing
@@ -48,17 +83,17 @@ def main():
     (dataDimension, numClasses, Xtrain, Ytrain, Xtest, Ytest,
      mean, std) = helpermethods.preProcessData(dataDir)
 
-    assert dataDimension % inputDims == 0, "Infeasible per step input, " + \
-        "Timesteps have to be integer"
+    # assert dataDimension % inputDims == 0, "Infeasible per step input, " + \
+    #     "Timesteps have to be integer"
 
-    X = tf.compat.v1.placeholder(
-        "float", [None, int(dataDimension / inputDims), inputDims])
-    Y = tf.compat.v1.placeholder("float", [None, numClasses])
+    # X = tf.compat.v1.placeholder(
+    #     "float", [None, int(dataDimension / inputDims), inputDims])
+    # Y = tf.compat.v1.placeholder("float", [None, numClasses])
 
-    currDir = helpermethods.createTimeStampDir(dataDir, cell)
+    # currDir = helpermethods.createTimeStampDir(dataDir, cell)
 
-    helpermethods.dumpCommand(sys.argv, currDir)
-    helpermethods.saveMeanStd(mean, std, currDir)
+    # helpermethods.dumpCommand(sys.argv, currDir)
+    # helpermethods.saveMeanStd(mean, std, currDir)
 
     if cell == "FastGRNN":
         FastCell = FastGRNNCell(hiddenDims,
@@ -84,18 +119,22 @@ def main():
     else:
         sys.exit('Exiting: No Such Cell as ' + cell)
 
-    FastCellTrainer = FastTrainer(
-        FastCell, X, Y, sW=sW, sU=sU,
-        learningRate=learningRate, outFile=outFile)
+    # FastCellTrainer = FastTrainer(
+    #     FastCell, X, Y, sW=sW, sU=sU,
+    #     learningRate=learningRate, outFile=outFile)
 
-    sess = tf.compat.v1.InteractiveSession()
-    sess.run(tf.compat.v1.global_variables_initializer())
+    # sess = tf.compat.v1.InteractiveSession()
+    # sess.run(tf.compat.v1.global_variables_initializer())
 
-    FastCellTrainer.train(batchSize, totalEpochs, sess, Xtrain, Xtest,
-                          Ytrain, Ytest, decayStep, decayRate,
-                          dataDir, currDir)
-
+    # FastCellTrainer.train(batchSize, totalEpochs, sess, Xtrain, Xtest,
+    #                       Ytrain, Ytest, decayStep, decayRate,
+    #                       dataDir, currDir)
     
+    # logits, _, _ = FastCellTrainer.computeGraph()
+    # model = tf.keras.Model(logits)
+
+    my_dir = "/home/krantikiran/EdgeML/examples/tf/FastCells/Google-30/FastGRNNResults/2021-03-23T23-49-52"
+    os.chdir(my_dir)
     idx = 0
     if FastCell._num_weight_matrices[0] == 1:
         # Vars.append(self.W)
@@ -104,8 +143,8 @@ def main():
     else:
         # Vars.extend([self.W1, self.W2])
         idx = idx + 2
-        FastCell.W1 = FastCellTrainer.FastParams[0]
-        FastCell.W2 = FastCellTrainer.FastParams[1]
+        FastCell.W1 = tf.convert_to_tensor(np.load("W1.npy"), dtype=tf.float32)# FastCellTrainer.FastParams[0]
+        FastCell.W2 = tf.convert_to_tensor(np.load("W2.npy"), dtype=tf.float32)# FastCellTrainer.FastParams[1]
 
 
     if FastCell._num_weight_matrices[1] == 1:
@@ -115,18 +154,22 @@ def main():
     else:
         # Vars.extend([self.U1, self.U2])
         idx = idx + 2
-        FastCell.U1 = FastCellTrainer.FastParams[2]
-        FastCell.U2 = FastCellTrainer.FastParams[3]
+        FastCell.U1 = tf.convert_to_tensor(np.load("U1.npy"), dtype=tf.float32)# FastCellTrainer.FastParams[2]
+        FastCell.U2 = tf.convert_to_tensor(np.load("U2.npy"), dtype=tf.float32)# FastCellTrainer.FastParams[3]
 
-    FastCell.bias_gate = FastCellTrainer.FastParams[idx]
-    FastCell.bias_update = FastCellTrainer.FastParams[idx + 1]
+    FastCell.bias_gate = tf.convert_to_tensor(np.load("Bg.npy"), dtype=tf.float32)# FastCellTrainer.FastParams[idx]
+    FastCell.bias_update = tf.convert_to_tensor(np.load("Bh.npy"), dtype=tf.float32)# FastCellTrainer.FastParams[idx + 1]
 
-    FastCell.zeta = FastCellTrainer.FastParams[idx + 2]
-    FastCell.nu = FastCellTrainer.FastParams[idx + 3]
+    FastCell.zeta = tf.convert_to_tensor(np.load("zeta.npy"), dtype=tf.float32)# FastCellTrainer.FastParams[idx + 2]
+    FastCell.nu = tf.convert_to_tensor(np.load("nu.npy"), dtype=tf.float32)# FastCellTrainer.FastParams[idx + 3]
 
-    model = tf.keras.Sequential(FastCell)
 
-    tf.saved_model.save(model, 'model')
+    # model = tf.keras.Sequential(FastCell, FastCellTrainer.FC, FastCellTrainer.FCbias)
+    # input_test = tf.convert_to_tensor(np.reshape(Xtest[0], (3168, 1)), dtype=tf.float32)
+    model = FastGRNNPredictor(FastCell, tf.convert_to_tensor(np.load("FC.npy"), dtype=tf.float32), tf.convert_to_tensor(np.load("FCbias.npy"), dtype=tf.float32))
+    model.compile(optimizer='adam')
+    model.fit(x=Xtrain, y=Ytrain)
+    model.save('model')
 
 
 if __name__ == '__main__':
